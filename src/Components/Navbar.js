4c487e9b89c43bdf9fa87105d11f222e2294232c
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-07-26 21:36:16 +0200

    add --> add linc to the button contact me

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
         <nav>
             <div className='flex flex-row justify-between group'>
                 <a href="https://moustafa-dev.com/"><img className='w-40 mt-3 ml-20' src={ logo } alt='logo'/></a>
-                <a href="" className='bg-orange-500 rounded-md w-40 flex justify-center items-center mr-20 mt-3 hover:bg-gray-950 group-hover:text-white ease-in duration-300'>
+                <a href="https://moustafa-dev.com/contact-us" className='bg-orange-500 rounded-md w-40 flex justify-center items-center mr-20 mt-3 hover:bg-gray-950 group-hover:text-white ease-in duration-300'>
                     Contattami
                 </a>
             </div>
