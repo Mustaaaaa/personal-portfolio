badf4d5d97c7cee234f23cca33af223af6f261ae
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-07-31 19:31:08 +0200

    add --> scroll disable function

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import MainContent from './Components/MainContent'
 import Footer from './Components/Footer'
 import AboutMe from './Components/AboutMe'
 import MyProjects from './Components/MyProjects'
+import { ScrollProvider } from './Components/Assets/ScrollDisable';
 
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<MainContent />} />
-          <Route path="/aboutme" element={<AboutMe />} />
-          <Route path="/myprojects" element={<MyProjects />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <ScrollProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+            <Route path="/aboutme" element={<AboutMe />} />
+            <Route path="/myprojects" element={<MyProjects />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </ScrollProvider>
+
     </>
   )
 
