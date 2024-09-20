e7e4201aa8570e339f70c25795c844ab49be1d8b
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-09-20 09:40:35 +0200

    add: deleted the usless script

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -69,26 +69,6 @@ function MainContent() {
         scrollToSection();
     }, [location]);
 
-    // elements
-    var elements_to_watch = document.querySelectorAll('.watch');
-
-    // callback
-    var callback = function (items) {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                item.target.classList.add("in-page");
-            } else {
-                item.target.classList.remove("in-page");
-            }
-        });
-    }
-    // observer
-    var observer = new IntersectionObserver(callback, { threshold: 0.6 });
-
-    // apply
-    elements_to_watch.forEach((element) => {
-        observer.observe(element);
-    });
 
     return (
 
