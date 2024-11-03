3d05a5d0b36acbc7fbbd2412dda5443a91ffd7a0
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-11-03 22:07:58 +0100

    add: deleted the routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar'
 import MainContent from './Components/MainContent'
 import Footer from './Components/Footer'
@@ -8,13 +7,9 @@ import Footer from './Components/Footer'
 function App() {
   return (
     <>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<MainContent />} />
-          </Routes>
-          <Footer />
-        </Router>
+      <Navbar />
+      <MainContent />
+      <Footer />
 
     </>
   )
