b449b703b6ae5264b69129369f11945f3cb33682
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-11-09 20:44:53 +0100

    add: make a responsive home section done

--- src/Components/Assets/HomeSection.jsx
+++ src/Components/Assets/HomeSection.jsx
@@ -22,34 +22,35 @@ const HomeSection = () => {
     gsap.registerPlugin(ScrollTrigger);
 
     useGSAP(() => {
-        let tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: '.clip-path-23',
-                pin: true,
-                start: 'top top',
-                end: '+=' + window.innerHeight * 1.3,
-                scrub: 0.6,
-            }
-        })
-
-        tl.to('.clip-path-23', {
-            rotation: -360,
-            xPercent: -110,
-            yPercent: 25,
-            backgroundColor: 'transparent',
-            clipPath: 'circle(50% at 50% 50%)',
-        })
-
-        tl.to('.clip-path-23', {
-            yPercent: 10,
-        })
-
-    })
+        if (window.innerWidth >= 1024) {
+            let tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.clip-path-23',
+                    pin: true,
+                    start: 'top top',
+                    end: '+=' + window.innerHeight * 1.3,
+                    scrub: 0.6,
+                }
+            });
+    
+            tl.to('.clip-path-23', {
+                rotation: -360,
+                xPercent: -110,
+                yPercent: 25,
+                backgroundColor: 'transparent',
+                clipPath: 'circle(50% at 50% 50%)',
+            });
+    
+            tl.to('.clip-path-23', {
+                yPercent: 10,
+            });
+        }
+    });
     return (
         <div className="flex w-full justify-center flex-col items-center">
 
-            <div className=" h-[calc(90vh)] max-w-[calc(1920px)] w-full z-10  flex lg:flex-row flex-col items-center justify-center">
-                <div className="w-3/6 flex flex-col items-center lg:items-start p-0 lg:pl-[calc(15%)]">
+            <div className="lg:h-[calc(90vh)] h-[calc(80vh)] lg:max-w-[calc(1920px)] w-full z-10  flex lg:flex-row flex-col-reverse items-center justify-center">
+                <div className="lg:w-3/6 w-full flex flex-col items-center lg:items-start lg:pl-[calc(15%)]">
                     <p data-aos="fade-down-right" className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl">Moustafa</p>
                     <p data-aos="fade-up-left" className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl xl:pl-64 lg:pl-44">Ibrahim</p>
                     <div className="text-sm flex sm:text-xl lg:text-2xl xl:text-3xl text-black " data-aos="fade-up">
@@ -67,7 +68,7 @@ const HomeSection = () => {
                     </div>
                     <div className='mt-10 flex flex-nowrap justify-center items-center' data-aos="fade-up">
                         <div>
-                            <a href={cv} download="moustafa_ibrahim_cv.pdf" className='text-lg h-14 w-44 border-2 flex justify-center items-center gap-2 rounded-full border-orange-500 text-orange-500  hover:bg-orange-500 hover:text-white transition duration-500 mr-6'><FontAwesomeIcon icon={faDownload} /> Scarica CV</a>
+                            <a href={cv} download="moustafa_ibrahim_cv.pdf" className='text-lg lg:h-14 lg:w-44 w-36 h-12 border-2 flex justify-center items-center gap-2 rounded-full border-orange-500 text-orange-500  hover:bg-orange-500 hover:text-white transition duration-500 mr-6'><FontAwesomeIcon icon={faDownload} /> Scarica CV</a>
                         </div>
                         <div className='group'>
                             <span className="absolute top-full left-[calc(59%)]  text-white bg-black rounded-md p-2 text-xs opacity-0 group-hover:opacity-100 duration-500">Github</span>
@@ -79,8 +80,8 @@ const HomeSection = () => {
                         </div>
                     </div>
                 </div>
-                <div className="lg:w-3/6 flex justify-center items-center clip-path-23 bg-orange-500 bg-no-repeat h-full top-0 right-0">
-                    <img src={imgPersonal} alt='imgDashboard' className='rounded-full ' />
+                <div className="mt-10 lg:my-0 lg:w-3/6 flex justify-center rounded-full h-60 w-60  items-center clip-path-23 bg-orange-500 bg-no-repeat lg:h-full top-0 right-0">
+                    <img src={imgPersonal} alt='imgDashboard' className='rounded-full w-5/6 lg:w-3/6 xl:w-2/6' />
                 </div>
             </div>
             <Stats />
