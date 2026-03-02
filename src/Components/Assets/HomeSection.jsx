c4310c7500446f22e6eb403ed5a61f774956d1b9
Mustaaaaa <moustafaibrahim1512@gmail.com>
2026-03-02 15:27:18 +0100

    add: aggiornamento CV

--- src/Components/Assets/HomeSection.jsx
+++ src/Components/Assets/HomeSection.jsx
@@ -12,7 +12,7 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 
 import imgPersonal from '../../Assets/aboutMe/personalImage.png';
-import cv from '../../Assets/aboutMe/moustafa.ibrahim.121.pdf';
+import cv from '../../Assets/aboutMe/CV_Ibrahim_Moustafa.pdf';
 
 import Stats from './Components/Stats';
 import AirplaneFlyButton from './Components/AirplaneFlyButton';
